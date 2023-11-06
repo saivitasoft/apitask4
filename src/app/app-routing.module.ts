10c8b787e4e4da3d63373ac987a92fe1b194b012
saivitasoft <saikumarvita@gmail.com>
2023-11-06 21:04:31 +0530

    routing & router link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Task1Component } from './task1/task1.component';
+import { Task2Component } from './task2/task2.component';
+import { TaskdataComponent } from './taskdata/taskdata.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path:'task1',component:Task1Component},
+  {path:'task2',component:Task2Component},
+  {path:'task3',component:TaskdataComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
